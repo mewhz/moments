4ccfae28b813db58a827f3ecb6906ddbefd56456
mewhz <mewanghz@163.com>
2024-12-14 16:17:41 +0800

    Test

--- src/main/java/com/mewhz/moments/MomentsApplication.java
+++ src/main/java/com/mewhz/moments/MomentsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MomentsApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(MomentsApplication.class, args);
     }
